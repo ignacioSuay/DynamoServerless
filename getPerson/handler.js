ce82dd45ae8334d866d07058c6e2356d8308b42a
44087895 <ignacio.s.mas@noexternalmail.hsbc.com>
2018-04-06 12:05:55 +0100

    refactor handler

--- getPerson/handler.js
+++ getPerson/handler.js
@@ -1,39 +1,37 @@
 'use strict';
-var AWS = require('aws-sdk');
+const AWS = require('aws-sdk');
 AWS.config.update({region: 'eu-west-1'});
 
 function buildResponse(item) {
-    var response = {
+    return {
         "statusCode": 200,
         "body": JSON.stringify(item),
         "isBase64Encoded": false
     };
-    return response;
 }
 
 module.exports.getPerson = (event, context, callback) => {
 
     console.log("running lambda");
-    var ddb = new AWS.DynamoDB();
+    const ddb = new AWS.DynamoDB();
 
-    var params = {
+    const params = {
         TableName: 'Person',
         Key: {
             'id': {N: event.pathParameters.id}
         }
     };
-    var item, error;
+    let item, error;
     console.log("getting the  item");
     ddb.getItem(params, function (err, data) {
         if (err) {
-            console.log(err)
+            console.log(err);
             error = err;
         } else {
-            console.log(data)
+            console.log(data);
             item = data.Item
         }
-        var response = buildResponse(item);
-        callback(error, response);
+        callback(error, buildResponse(item));
     });
 
 };
