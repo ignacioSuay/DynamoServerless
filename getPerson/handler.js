146a330f112e4b5118c8f3fd25833a70e1538e28
44087895 <ignacio.s.mas@noexternalmail.hsbc.com>
2018-04-09 09:04:23 +0100

    refactor handler

--- getPerson/handler.js
+++ getPerson/handler.js
@@ -2,9 +2,9 @@
 const AWS = require('aws-sdk');
 AWS.config.update({region: 'eu-west-1'});
 
-function buildResponse(item) {
+function buildResponse(code, item) {
     return {
-        "statusCode": 200,
+        "statusCode": code,
         "body": JSON.stringify(item),
         "isBase64Encoded": false
     };
@@ -21,17 +21,14 @@ module.exports.getPerson = (event, context, callback) => {
             'id': {N: event.pathParameters.id}
         }
     };
-    let item, error;
-    console.log("getting the  item");
+
     ddb.getItem(params, function (err, data) {
         if (err) {
             console.log(err);
-            error = err;
-        } else {
-            console.log(data);
-            item = data.Item
+            callback(error, buildResponse(500, err));
         }
-        callback(error, buildResponse(item));
+
+        callback(error, buildResponse(200, data.Item));
     });
 
 };
