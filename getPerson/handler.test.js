94c1dcd6c0af9dc470504ea0567f382e1e1d5fe6
44087895 <ignacio.s.mas@noexternalmail.hsbc.com>
2018-04-09 16:41:25 +0100

    refactor test

--- getPerson/handler.test.js
+++ getPerson/handler.test.js
@@ -1,24 +1,25 @@
 const handler = require('./handler');
 const AWS = require('aws-sdk-mock');
 
-AWS.mock('DynamoDB', 'getItem', function (params, callback){
+
+AWS.mock('DynamoDB', 'getItem', (params, callback) => {
 
     const data = {Item: {id: '1', name: 'Ignacio', lastName: 'Suay'}}
 
-  callback(null, data);
+    callback(null, data);
 });
 
-test('adds 1 + 2 to equal 3', () => {
+test('test getPeson should return a person', function () {
 
-    const event = {pathParameters: {id: '1'}}
+    const event = {pathParameters: {id: '1'}};
 
-  handler.getPerson(event, null, (err, data) => {
-   console.log(data);
-   const body = JSON.parse(data.body)
+    handler.getPerson(event, null, (err, data) => {
+        console.log(data);
+        const body = JSON.parse(data.body)
 
-   expect(body.id).toBe('1');
-   expect(body.name).toBe('Ignacio');
-   expect(body.lastName).toBe('Suay');
-  });
+        expect(body.id).toBe('1');
+        expect(body.name).toBe('Ignacio');
+        expect(body.lastName).toBe('Suay');
+    });
 
 });
